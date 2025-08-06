82e5bc6872cffc2a25f4679020357e56cff04166
Eternal-Allies <eternalallies.au@gmail.com>
2025-08-06 22:11:27 +0530

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,35 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: true, // 👈 Add this line
+    host: false, //👈
     proxy: {
-      '/api': {
-        target: 'http://10.10.10.34:8081',
+      "/api": {
+        target: "http://10.10.10.34:8081",
         changeOrigin: true,
         secure: false,
         configure: (proxy, _options) => {
-          proxy.on('error', (err, _req, _res) => {
-            console.log('proxy error', err);
+          proxy.on("error", (err, _req, _res) => {
+            console.log("proxy error", err);
           });
-          proxy.on('proxyReq', (proxyReq, req, _res) => {
-            console.log('Sending Request to the Target:', req.method, req.url);
+          proxy.on("proxyReq", (proxyReq, req, _res) => {
+            console.log("Sending Request to the Target:", req.method, req.url);
           });
-          proxy.on('proxyRes', (proxyRes, req, _res) => {
-            console.log('Received Response from the Target:', proxyRes.statusCode, req.url);
+          proxy.on("proxyRes", (proxyRes, req, _res) => {
+            console.log(
+              "Received Response from the Target:",
+              proxyRes.statusCode,
+              req.url
+            );
           });
         },
-        rewrite: (path) => path.replace(/^\/api/, '/chatbot')
-      }
-    }
+        rewrite: (path) => path.replace(/^\/api/, "/chatbot"),
+      },
+    },
   },
   optimizeDeps: {
-    exclude: ['lucide-react'],
+    exclude: ["lucide-react"],
   },
 });
